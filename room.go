260f529430a5bbc0ab01769f09060ad1f0b600b8
pnrnc485 <kenta.suzuki1014@gmail.com>
2017-06-27 18:44:06 +0900

    チャットルームをHTTPハンドラにした

--- room.go
+++ room.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"syscall"
+	"github.com/gorilla/websocket"
+	"net/http"
+	"log"
+)
+
 type room struct {
 
 	//他のクライアントに転送するためのメッセージを保持するチャネル
@@ -13,4 +20,54 @@ type room struct {
 
 	//在室している全てのクライアントが保持される
 	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for  {
+		select {
+		case client := <-r.join:
+			//参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			//退室
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+
+			// 全てのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					//メッセージを送信
+				default:
+					//送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize 	= 1024
+	messageBufferSize 	= 256
+)
+
+var upgrade = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+	socket, err := upgrade.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+
+	client := &client{socket: socket, send: make(chan []byte, messageBufferSize), room: r, }
+
+	r.join <- client
+	defer  func () { r.leave <- client }()
+	go client.write()
+	client.read()
 }
