21d9bdd1f95f64a15d8005eea620dea80ddf23cc
pnrnc485 <kenta.suzuki1014@gmail.com>
2017-06-27 17:49:25 +0900

    チャットルームのモデル化

--- room.go
+++ room.go
@@ -4,4 +4,13 @@ type room struct {
 
 	//他のクライアントに転送するためのメッセージを保持するチャネル
 	forward chan []byte
+
+	//チャットルームに参加しようとしているクライアントのためのチャネル
+	join chan *client
+
+	//チャットルームから退室しようとしているクライアントのためのチャネル
+	leave chan *client
+
+	//在室している全てのクライアントが保持される
+	clients map[*client]bool
 }
