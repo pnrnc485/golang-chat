92e842e8cf54c86d10fca52e42285e9496c503a4
pnrnc485 <kenta.suzuki1014@gmail.com>
2017-06-27 16:37:45 +0900

    Webサーバ作成、テンプレートの仕組み構築

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"sync"
+	"html/template"
+	"path/filepath"
+)
+
+// temp1は一つのテンプレートを表します
+type templateHandler struct {
+	once		sync.Once
+	filename	string
+	temp1		*template.Template
+}
+
+//ServeHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.temp1 = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.temp1.Execute(w, nil) // 戻り値はチェックするべき
+}
+
+func main() {
+
+	//ルート
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	//webサーバを開始します
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
