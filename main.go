0e6b6a64ca28dcc6418e8f14d6c2cd5727da130e
pnrnc485 <kenta.suzuki1014@gmail.com>
2017-06-28 17:12:11 +0900

    テンプレートからHTMLを出力する際にRequestデータを参照できるようにした

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 	"html/template"
 	"path/filepath"
+	"flag"
 )
 
 // temp1は一つのテンプレートを表します
@@ -20,10 +21,15 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.temp1 = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.temp1.Execute(w, nil) // 戻り値はチェックするべき
+	t.temp1.Execute(w, r) // 戻り値はチェックするべき Request情報を添付する
 }
 
 func main() {
+
+	//ポート番号
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()// フラグを解釈する
+
 	r := newRoom()
 
 	//ルート
@@ -32,6 +38,7 @@ func main() {
 	go r.run()
 
 	//webサーバを開始します
+	log.Println("Webサーバを起動します。ポート番号: ", *addr)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
