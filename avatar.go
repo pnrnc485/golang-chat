6ae1672e23f68d25377fe7bef189fa84a9a8c189
pnrnc485 <kenta.suzuki1014@gmail.com>
2017-07-02 18:38:28 +0900

    ユーザの識別をメアドをハッシュ化したものをつかったuseridで行うようにした

--- avatar.go
+++ avatar.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"errors"
-	"crypto/md5"
-	"io"
-	"strings"
-	"fmt"
 )
 
 //ErrNoAvatarはAvatarインスタンスがアバターのURLを返すことができない場合に発生するエラーです
@@ -33,11 +29,9 @@ func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
 type GravatarAvatar struct { }
 var UseGravatar GravatarAvatar
 func (_ GravatarAvatar) GetAvatarURL(c *client) (string, error) {
-	if email, ok := c.userData["email"]; ok {
-		if emailStr, ok := email.(string); ok {
-			m := md5.New()//ハッシュ化
-			io.WriteString(m, strings.ToLower(emailStr))
-			return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
+	if userid, ok := c.userData["userid"]; ok {
+		if useridStr, ok := userid.(string); ok {
+			return "//www.gravatar.com/avatar/" + useridStr, nil
 		}
 	}
 
